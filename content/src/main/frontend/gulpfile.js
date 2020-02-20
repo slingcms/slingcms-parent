32538376de0034890e73557f77eec6be0c82cde5
wildone <max.barrass@gmail.com>
2020-02-20 22:53:36 +1100

    remove license crum

--- content/src/main/frontend/gulpfile.js
+++ content/src/main/frontend/gulpfile.js
@@ -5,26 +5,6 @@ const cleanCSS   = require('gulp-clean-css');
 const rename =  require('gulp-rename');
 var concatCss = require('gulp-concat-css');
 
-const apache2License = [
-'/*',
-' * Licensed to the Apache Software Foundation (ASF) under one or more',
-' * contributor license agreements.  See the NOTICE file distributed with',
-' * this work for additional information regarding copyright ownership.',
-' * The ASF licenses this file to You under the Apache License, Version 2.0',
-' * (the "License"); you may not use this file except in compliance with',
-' * the License.  You may obtain a copy of the License at',
-' *',
-' *      http://www.apache.org/licenses/LICENSE-2.0',
-' *',
-' * Unless required by applicable law or agreed to in writing, software',
-' * distributed under the License is distributed on an "AS IS" BASIS,',
-' * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.',
-' * See the License for the specific language governing permissions and',
-' * limitations under the License.',
-' */',
-''
-].join('\n');
-
 function srcDir(sub) {
     return `./src/${sub}`;
 }
