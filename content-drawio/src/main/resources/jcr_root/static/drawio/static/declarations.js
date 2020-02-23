5f55c6d041b8847f2cdd514ea666a1fcd097f90d
wildone <max.barrass@gmail.com>
2020-02-24 09:00:52 +1100

    add drawio source

---
+++ content-drawio/src/main/resources/jcr_root/static/drawio/static/declarations.js
@@ -0,0 +1,4 @@
+window.STENCIL_PATH = window.STENCIL_PATH || '/static/drawio/js/stencils';
+window.SHAPES_PATH = window.SHAPES_PATH || '/static/drawio/js/shapes';
+window.mxBasePath = window.mxBasePath || '/static/drawio/js/mxgraph/';
+window.mxLoadStylesheets = window.mxLoadStylesheets || false;
